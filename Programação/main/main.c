282f6868003065527edbf3f8be3ebcf7fb5d39d9
luiz-sene <48630652+luiz-sene@users.noreply.github.com>
2022-07-28 20:18:53 -0300

    Update main.c

--- Programação/main/main.c
+++ Programação/main/main.c
@@ -1,20 +1,3 @@
-/*
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-/****************************************************************************
-*
-* This demo showcases creating a GATT database using a predefined attribute table.
-* It acts as a GATT server and can send adv data, be connected by client.
-* Run the gatt_client demo, the client demo will automatically connect to the gatt_server_service_table demo.
-* Client demo will enable GATT server's notify after connection. The two devices will then exchange
-* data.
-*
-****************************************************************************/
 #include "pwm.h"
 #include "sensor.h"
 #include "servomotor.h"
